7ab75029fc9ed8e7fefc6f99b6a4883e5d60b917
jisan5620jk <jisank5620@gmail.com>
2024-07-02 11:23:39 +0600

    Update Code

--- src/Pages/Home1/Home1.jsx
+++ src/Pages/Home1/Home1.jsx
@@ -1,11 +1,13 @@
 import About from "../../Componant1/About/About";
 import Banner from "../../Componant1/Banner/Banner";
+import Contact from "../../Componant1/Contact/Contact";
 import Counter from "../../Componant1/Counter/Counter";
 import Feature from "../../Componant1/Feature/Feature";
 import Process from "../../Componant1/Process/Process";
 import Service from "../../Componant1/Service/Service";
 import TeamMember from "../../Componant1/TeamMember/TeamMember";
 import Testimonial from "../../Componant1/Testimonial/Testimonial";
+import Video from "../../Componant1/Video/Video";
 
 const Home1 = () => {
   return (
@@ -18,6 +20,8 @@ const Home1 = () => {
       <Counter />
       <Process />
       <Testimonial />
+      <Video />
+      <Contact />
     </>
   );
 };
