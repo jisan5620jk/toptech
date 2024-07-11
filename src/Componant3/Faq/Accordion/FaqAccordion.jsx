3d152ca14d82cd0dcb1702189e32b5571b5e59eb
jisan5620jk <jisank5620@gmail.com>
2024-07-11 15:47:58 +0600

    Update Code

--- src/Componant3/Faq/Accordion/FaqAccordion.jsx
+++ src/Componant3/Faq/Accordion/FaqAccordion.jsx
@@ -13,7 +13,7 @@ const PricingAccordion = ({ children, faqIcon, title, id, active = false }) => {
     <div className="mb-5 rounded-md overflow-hidden">
       <h2>
         <button
-          className="faq-button rounded-md flex items-center justify-between pl-4 pr-2 md:pl-7 md:pr-6 lg:pl-6 lg:pr-4 xl:pl-7 xl:pr-6 py-5 w-full text-left font-medium font-Nunito transition-all duration-500 group bg-[#cedef7] border-b border-transparent"
+          className="faq-button2 rounded-md flex items-center justify-between pl-4 pr-2 md:pl-7 md:pr-6 lg:pl-6 lg:pr-4 xl:pl-7 xl:pr-6 py-5 w-full text-left font-medium font-Nunito transition-all duration-500 group bg-[#cedef7] border-b border-transparent"
           onClick={(e) => {
             e.preventDefault();
             setAccordionOpen(!accordionOpen);
