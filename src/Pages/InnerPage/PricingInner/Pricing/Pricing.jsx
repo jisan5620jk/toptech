0e259d821278b42519397ec89d0386e0ae28c223
jisan5620jk <jisank5620@gmail.com>
2024-08-03 17:48:09 +0600

    Update Code

--- src/Pages/InnerPage/PricingInner/Pricing/Pricing.jsx
+++ src/Pages/InnerPage/PricingInner/Pricing/Pricing.jsx
@@ -1,57 +1,104 @@
-/* eslint-disable no-unused-vars */
-
-import { FaCircle, FaStarOfLife } from "react-icons/fa6";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import PricingCard from "./PricingCard";
 
 const pricingData = [
   {
     id: 1,
     currency: "$",
-    offerPrice: 39,
-    pricingDateLine: "Month",
-    pricingDesc:
-      "Fast project turnaround time, substantial cost savings & quality standards.",
-    pricingTitle: "Starter Plans",
-    pricingIcon: <FaCircle />,
-    pricingContent1: "Repair & Replacement",
-    pricingContent2: "Reliability And Performance",
-    pricingContent3: "Real Solar PV Systems",
-    pricingContent4: "Just-In-Time Manufacturing",
+    offerPrice: 29,
+    mainPrice: "$39",
+    pricingDateLine: "/Monthly",
+    pricingTitle: "Basic Plans",
+    pricingSubTitle: "Small Team",
+    pricingContent1: "Email & Communication Services",
+    pricingContent2: "Implementing measures encryption.",
+    pricingContent3: "Standard Security Measuresimg",
+    pricingContent4: "Network and security management",
+    pricingContent5: "Helps understand past performance.",
     pricingUrl: "/pricing_inner",
-    pricingBtn: "Choose Plan",
-    
+    pricingBtn: "Get Started",
   },
   {
     id: 2,
     currency: "$",
     offerPrice: 59,
-    pricingDateLine: "Month",
-    pricingDesc:
-      "Fast project turnaround time, substantial cost savings & quality standards.",
+    mainPrice: "$69",
+    pricingDateLine: "/Monthly",
     pricingTitle: "Standard Plans",
-    pricingIcon: <FaCircle />,
-    pricingContent1: "Repair & Replacement",
-    pricingContent2: "Reliability And Performance",
-    pricingContent3: "Real Solar PV Systems",
-    pricingContent4: "Just-In-Time Manufacturing",
+    pricingSubTitle: "Medium Team",
+    pricingContent1: "Email & Communication Services",
+    pricingContent2: "Implementing measures encryption.",
+    pricingContent3: "Standard Security Measuresimg",
+    pricingContent4: "Network and security management",
+    pricingContent5: "Helps understand past performance.",
     pricingUrl: "/pricing_inner",
-    pricingBtn: "Choose Plan",
+    pricingBtn: "Get Started",
   },
   {
     id: 3,
     currency: "$",
     offerPrice: 89,
-    pricingDateLine: "Month",
-    pricingDesc:
-      "Fast project turnaround time, substantial cost savings & quality standards.",
+    mainPrice: "$99",
+    pricingDateLine: "/Monthly",
     pricingTitle: "Premium Plans",
-    pricingIcon: <FaCircle />,
-    pricingContent1: "Repair & Replacement",
-    pricingContent2: "Reliability And Performance",
-    pricingContent3: "Real Solar PV Systems",
-    pricingContent4: "Just-In-Time Manufacturing",
+    pricingSubTitle: "Large Team",
+    pricingContent1: "Email & Communication Services",
+    pricingContent2: "Implementing measures encryption.",
+    pricingContent3: "Standard Security Measuresimg",
+    pricingContent4: "Network and security management",
+    pricingContent5: "Helps understand past performance.",
     pricingUrl: "/pricing_inner",
-    pricingBtn: "Choose Plan",
+    pricingBtn: "Get Started",
+  },
+];
+const pricingData2 = [
+  {
+    id: 1,
+    currency: "$",
+    offerPrice: 29,
+    mainPrice: "$39",
+    pricingDateLine: "/Yearly",
+    pricingTitle: "Basic Plans",
+    pricingSubTitle: "Small Team",
+    pricingContent1: "Email & Communication Services",
+    pricingContent2: "Implementing measures encryption.",
+    pricingContent3: "Standard Security Measuresimg",
+    pricingContent4: "Network and security management",
+    pricingContent5: "Helps understand past performance.",
+    pricingUrl: "/pricing_inner",
+    pricingBtn: "Get Started",
+  },
+  {
+    id: 2,
+    currency: "$",
+    offerPrice: 59,
+    mainPrice: "$69",
+    pricingDateLine: "/Yearly",
+    pricingTitle: "Standard Plans",
+    pricingSubTitle: "Medium Team",
+    pricingContent1: "Email & Communication Services",
+    pricingContent2: "Implementing measures encryption.",
+    pricingContent3: "Standard Security Measuresimg",
+    pricingContent4: "Network and security management",
+    pricingContent5: "Helps understand past performance.",
+    pricingUrl: "/pricing_inner",
+    pricingBtn: "Get Started",
+  },
+  {
+    id: 3,
+    currency: "$",
+    offerPrice: 89,
+    mainPrice: "$99",
+    pricingDateLine: "/Yearly",
+    pricingTitle: "Premium Plans",
+    pricingSubTitle: "Large Team",
+    pricingContent1: "Email & Communication Services",
+    pricingContent2: "Implementing measures encryption.",
+    pricingContent3: "Standard Security Measuresimg",
+    pricingContent4: "Network and security management",
+    pricingContent5: "Helps understand past performance.",
+    pricingUrl: "/pricing_inner",
+    pricingBtn: "Get Started",
   },
 ];
 
@@ -59,56 +106,116 @@ const Pricing = () => {
   return (
     <section className="py-28 bg-BodyBg-0">
       <div className="Container">
-        <div className="text-center">
-          <h5 className="font-Nunito font-medium text-PrimaryColor-0 flex items-center justify-center gap-2">
-            <FaStarOfLife />
-            PRICING PLAN <FaStarOfLife />
-          </h5>
-          <h1 className="font-Nunito font-bold text-[22px] leading-8 sm:text-[38px] sm:leading-[48px] md:text-[44px] md:leading-[54px] lg:text-[32px] lg:leading-[42px] xl:text-[40px] xl:leading-[50px] 2xl:text-[46px] 2xl:leading-[56px] text-HeadingColor-0 mt-2 mb-6">
-            A Customizable Approach
-            <br /> for Your Business
-          </h1>
-        </div>
-        <div className="mt-12">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
-            {pricingData.map(
-              ({
-                id,
-                currency,
-                offerPrice,
-                pricingDateLine,
-                pricingDesc,
-                pricingTitle,
-                pricingIcon,
-                pricingContent1,
-                pricingContent2,
-                pricingContent3,
-                pricingContent4,
-                pricingUrl,
-                pricingBtn,
-              }) => {
-                return (
-                  <div key={id}>
-                    <PricingCard
-                      currency={currency}
-                      offerPrice={offerPrice}
-                      pricingDateLine={pricingDateLine}
-                      pricingDesc={pricingDesc}
-                      pricingTitle={pricingTitle}
-                      pricingIcon={pricingIcon}
-                      pricingContent1={pricingContent1}
-                      pricingContent2={pricingContent2}
-                      pricingContent3={pricingContent3}
-                      pricingContent4={pricingContent4}
-                      pricingUrl={pricingUrl}
-                      pricingBtn={pricingBtn}
-                    />
-                  </div>
-                );
-              }
-            )}
+        <div className="grid grid-cols-2 items-center">
+          <div>
+            <h1 className="font-Rajdhani font-bold text-lg leading-7 sm:text-[34px] sm:leading-[44px] md:text-[44px] md:leading-[54px] lg:text-[28px] lg:leading-[38px] xl:text-[37px] xl:leading-[47px] 2xl:text-[37px] 2xl:leading-[47px] text-HeadingColor-0">
+              Choose Your Best Plan
+            </h1>
+            <p className="font-Nunito text-TextColor2-0 mt-3">
+              Services are professional offerings provided by businesses to meet
+              specific needs or solve <br className="hidden 2xl:block" />{" "}
+              problems for their customers. Services can range from your
+              services.
+            </p>
           </div>
+          <div></div>
         </div>
+        <Tabs>
+          <div className="flex items-end flex-col">
+            <TabList className="flex items-center justify-end -mt-20 gap-x-2 w-[361px] border border-BorderColor2-0 p-[6px]  rounded-md overflow-hidden">
+              <Tab className="cursor-pointer outline-none bg-SecondaryColor-0 px-9 py-[14px] rounded-md font-Nunito text-white transition-all duration-500 hover:bg-PrimaryColor-0">
+                Billed Monthly
+              </Tab>
+              <Tab className="cursor-pointer outline-none bg-PrimaryColor-0 px-9 py-[14px] rounded-md font-Nunito text-white transition-all duration-500 hover:bg-SecondaryColor-0">
+                Billed Yearly
+              </Tab>
+            </TabList>
+          </div>
+          <TabPanel>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-[56px]">
+              {pricingData.map(
+                ({
+                  id,
+                  currency,
+                  offerPrice,
+                  mainPrice,
+                  pricingDateLine,
+                  pricingTitle,
+                  pricingSubTitle,
+                  pricingContent1,
+                  pricingContent2,
+                  pricingContent3,
+                  pricingContent4,
+                  pricingContent5,
+                  pricingUrl,
+                  pricingBtn,
+                }) => {
+                  return (
+                    <div key={id}>
+                      <PricingCard
+                        currency={currency}
+                        offerPrice={offerPrice}
+                        mainPrice={mainPrice}
+                        pricingDateLine={pricingDateLine}
+                        pricingTitle={pricingTitle}
+                        pricingSubTitle={pricingSubTitle}
+                        pricingContent1={pricingContent1}
+                        pricingContent2={pricingContent2}
+                        pricingContent3={pricingContent3}
+                        pricingContent4={pricingContent4}
+                        pricingContent5={pricingContent5}
+                        pricingUrl={pricingUrl}
+                        pricingBtn={pricingBtn}
+                      />
+                    </div>
+                  );
+                }
+              )}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-[72px]">
+              {pricingData2.map(
+                ({
+                  id,
+                  currency,
+                  offerPrice,
+                  mainPrice,
+                  pricingDateLine,
+                  pricingTitle,
+                  pricingSubTitle,
+                  pricingContent1,
+                  pricingContent2,
+                  pricingContent3,
+                  pricingContent4,
+                  pricingContent5,
+                  pricingUrl,
+                  pricingBtn,
+                }) => {
+                  return (
+                    <div key={id}>
+                      <PricingCard
+                        currency={currency}
+                        offerPrice={offerPrice}
+                        mainPrice={mainPrice}
+                        pricingDateLine={pricingDateLine}
+                        pricingTitle={pricingTitle}
+                        pricingSubTitle={pricingSubTitle}
+                        pricingContent1={pricingContent1}
+                        pricingContent2={pricingContent2}
+                        pricingContent3={pricingContent3}
+                        pricingContent4={pricingContent4}
+                        pricingContent5={pricingContent5}
+                        pricingUrl={pricingUrl}
+                        pricingBtn={pricingBtn}
+                      />
+                    </div>
+                  );
+                }
+              )}
+            </div>
+          </TabPanel>
+        </Tabs>
       </div>
     </section>
   );
--- src/Pages/InnerPage/PricingInner/Pricing/PricingCard.jsx
+++ src/Pages/InnerPage/PricingInner/Pricing/PricingCard.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import CountUp from "react-countup";
-import { FaArrowRightLong } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 const PricingCard = ({
@@ -19,16 +18,16 @@ const PricingCard = ({
   pricingSubTitle,
 }) => {
   return (
-    <div className="bg-white relative z-10 border-2 border-white">
-      <div className="bg-[url(/images/pricing-head-bg.png)] bg-no-repeat bg-cover bg-center">
-        <h4 className="font-Rajdhani font-bold text-[32px] text-PrimaryColor-0 inline-block transition-all duration-500 mt-10">
+    <div className="bg-white relative z-10 border-2 border-white !w-full">
+      <div className="bg-[url(/images/pricing-head-bg.png)] bg-no-repeat bg-cover bg-center px-7 2xl:px-9">
+        <h4 className="font-Rajdhani font-bold text-[32px] text-HeadingColor-0 inline-block transition-all duration-500 mt-6">
           {pricingTitle}
         </h4>
-        <p className="font-Nunito text-TextColor2-0">{pricingSubTitle}</p>
+        <p className="font-Nunito text-TextColor2-0 pb-8">{pricingSubTitle}</p>
       </div>
-      <div className="font-Nunito flex items-baseline gap-3 px-[30px] mt-6 pb-4">
+      <div className="font-Nunito flex items-baseline gap-3 px-7 2xl:px-9 mt-5">
         <div className="flex items-center gap-1">
-          <h6 className="font-Nunito font-bold text-[50px] text-HeadingColor-0">
+          <h6 className="font-Rajdhani font-bold text-[55px] text-HeadingColor-0">
             {currency}
           </h6>
           <CountUp
@@ -38,40 +37,39 @@ const PricingCard = ({
             className="text-[55px] font-bold font-Rajdhani text-HeadingColor-0"
           />
         </div>
-        <div className="line-through">
-          <h6 className="font-Rajdhani font-semibold text-[22px] text-PrimaryColor-0">
+        <div className="line-through text-PrimaryColor-0 flex">
+          <h6 className="font-Rajdhani font-semibold text-[22px] text-PrimaryColor-0 pr-1">
             {mainPrice}
           </h6>
-          <p className="ont-Rajdhani font-semibold text-[22px] text-PrimaryColor-0">
+          <p className="font-Rajdhani font-semibold text-[22px] text-PrimaryColor-0">
             {pricingDateLine}
           </p>
         </div>
       </div>
-      <ul>
-        <li className="flex gap-2 items-center font-Nunito text-TextColor-0 px-[30px] mb-3 mt-9">
+      <ul className="px-7 xl:px-9 mt-4">
+        <li className="flex gap-2 items-center font-Nunito text-TextColor2-0 mb-5">
           {pricingContent1}
         </li>
-        <li className="flex gap-2 items-center font-Nunito text-TextColor-0 px-[30px] mb-3">
+        <li className="flex gap-2 items-center font-Nunito text-TextColor2-0 mb-5">
           {pricingContent2}
         </li>
-        <li className="flex gap-2 items-center font-Nunito text-TextColor-0 px-[30px] mb-3">
+        <li className="flex gap-2 items-center font-Nunito text-TextColor2-0 mb-5">
           {pricingContent3}
         </li>
-        <li className="flex gap-2 items-center font-Nunito text-TextColor-0 px-[30px] mb-1">
+        <li className="flex gap-2 items-center font-Nunito text-TextColor2-0 mb-5">
           {pricingContent4}
         </li>
-        <li className="flex gap-2 items-center font-Nunito text-TextColor-0  px-[30px] mb-1">
+        <li className="flex gap-2 items-center font-Nunito text-TextColor2-0 mb-5">
           {pricingContent4}
         </li>
-        <li className="flex gap-2 items-center font-Nunito text-TextColor-0 px-[30px] mb-1">
+        <li className="flex gap-2 items-center font-Nunito text-TextColor-0 line-through">
           {pricingContent5}
         </li>
       </ul>
-      <div className="px-[30px] pb-10 flex justify-center">
+      <div className="pb-12 pt-3 flex justify-center">
         <Link to={pricingUrl}>
-          <button className="primary-btn2 mt-9">
+          <button className="primary-btn2 !py-4 mt-9">
             {pricingBtn}
-            <FaArrowRightLong size={"20"} />
           </button>
         </Link>
       </div>
