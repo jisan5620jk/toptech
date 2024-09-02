8e4fa4dc267cae73d7b0be0609cff952f08f9e9d
jisan5620jk <jisank5620@gmail.com>
2024-09-02 17:03:11 +0600

    Add Some Component

--- src/Pages/Home7/Home7.jsx
+++ src/Pages/Home7/Home7.jsx
@@ -1,9 +1,15 @@
 import Banner from "../../Component7/Banner/Banner";
+import Benifits from "../../Component7/Benifits/Benifits";
+import Collaboration from "../../Component7/Collaboration/Collaboration";
+import Feature from "../../Component7/Feature/Feature";
 
 const Home7 = () => {
   return (
     <>
       <Banner />
+      <Feature />
+      <Collaboration />
+      <Benifits/>
     </>
   );
 };
